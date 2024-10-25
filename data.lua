9bdb086ce61c11d193a04e421ecc73f212572d94
blackklotus <lucasceratto@gmail.com>
2024-10-25 11:30:44 -0300

    update mod name

--- data.lua
+++ data.lua
@@ -1,12 +1,15 @@
 data:extend {
-  { type = "sprite", name = "ammo-icon-white",
-    filename = "__Nearby-Ammo-Count__/ammo-icon-white.png",
-    priority = "extra-high",---no-scale",
+  {
+    type = "sprite",
+    name = "ammo-icon-white",
+    filename = "__Nearby-Ammo-Count-2.0__/ammo-icon-white.png",
+    priority = "extra-high",
+    ---no-scale",
     width = 64,
     height = 64,
---	apply_runtime_tint = true,
+    -- apply_runtime_tint = true,
     flags = {"gui-icon"},
---  mipmap_count = 2,
+    -- mipmap_count = 2,
     scale = 0.5
   },
 }
